aa0437766f0606516019ee6a886869509ba931c5
H/S <kidoairakusyunkasyuutou@gmail.com>
2024-11-09 17:11:07 +0900

    File.exists is obsoleted.

--- lib/oauth/models/consumers/service_loader.rb
+++ lib/oauth/models/consumers/service_loader.rb
@@ -11,7 +11,7 @@ if defined? ConsumerToken && defined? OAUTH_CREDENTIALS
   OAUTH_CREDENTIALS.each do |key, value|
     class_name=value[:class_name]||"#{key.to_s.classify}Token"
     unless Object.const_defined?(class_name.to_sym)
-      if File.exists?(File.join(File.dirname(__FILE__), "services","#{key.to_s}_token.rb"))
+      if File.exist?(File.join(File.dirname(__FILE__), "services","#{key.to_s}_token.rb"))
         Rails.logger.info File.join(File.dirname(__FILE__), "services","#{key.to_s}_token")
         require File.join(File.dirname(__FILE__), "services","#{key.to_s}_token")
       else
